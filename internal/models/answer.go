44dc7f5fb5715cb78947a04803b6fded8521260e
Arif Rahman Hakim <new.arifrh199@gmail.com>
2026-06-18 10:11:46 +0800

    feat: add foreign key relationships for Assignment in Answer, Location, and Log models

--- internal/models/answer.go
+++ internal/models/answer.go
@@ -7,10 +7,12 @@ import (
 )
 
 type Answer struct {
-	ID           string `gorm:"type:uuid;default:gen_random_uuid();primaryKey"`
-	AssignmentID string `gorm:"type:text;not null"` // 1c0e2e43-fb41-4d1f-bb52-3e6cded8c343
-	LocationID   *string `gorm:"type:uuid;index"`
-	Name         string `gorm:"type:text;not null"` // data1, data2, ..., data10
+	ID           string     `gorm:"type:uuid;default:gen_random_uuid();primaryKey"`
+	AssignmentID string     `gorm:"type:text;not null"` // 1c0e2e43-fb41-4d1f-bb52-3e6cded8c343
+	Assignment   Assignment `gorm:"foreignKey:AssignmentID;references:AssignmentID"`
+	LocationID   *string    `gorm:"type:uuid;index"`
+	Location     *Location  `gorm:"foreignKey:LocationID;references:ID"`
+	Name         string     `gorm:"type:text;not null"` // data1, data2, ..., data10
 	AnsweredAt   time.Time
 	RevisedAt    time.Time
 	CreatedAt    time.Time
--- internal/models/location.go
+++ internal/models/location.go
@@ -7,15 +7,16 @@ import (
 )
 
 type Location struct {
-	ID                     string  `gorm:"type:uuid;default:gen_random_uuid();primaryKey"`
-	AssignmentID           string  `gorm:"type:text;not null;index:idx_assignment_canonical_id,priority:1"`
-	CanonicalID            string  `gorm:"type:text;not null;index:idx_assignment_canonical_id,priority:2"`
-	Latitude               float64 `gorm:"type:double precision;not null"`
-	Longitude              float64 `gorm:"type:double precision;not null"`
-	AnswerCount            int     `gorm:"not null;default:0"`
-	Proportion             float64 `gorm:"type:double precision;not null;default:0"`
-	DistanceToSampleMeters float64 `gorm:"type:double precision;not null;default:0"`
-	WithinSampleAreaRadius bool    `gorm:"not null;default:false"`
+	ID                     string     `gorm:"type:uuid;default:gen_random_uuid();primaryKey"`
+	AssignmentID           string     `gorm:"type:text;not null;index:idx_assignment_canonical_id,priority:1"`
+	Assignment             Assignment `gorm:"foreignKey:AssignmentID;references:AssignmentID"`
+	CanonicalID            string     `gorm:"type:text;not null;index:idx_assignment_canonical_id,priority:2"`
+	Latitude               float64    `gorm:"type:double precision;not null"`
+	Longitude              float64    `gorm:"type:double precision;not null"`
+	AnswerCount            int        `gorm:"not null;default:0"`
+	Proportion             float64    `gorm:"type:double precision;not null;default:0"`
+	DistanceToSampleMeters float64    `gorm:"type:double precision;not null;default:0"`
+	WithinSampleAreaRadius bool       `gorm:"not null;default:false"`
 	CreatedAt              time.Time
 	UpdatedAt              time.Time
 	DeletedAt              gorm.DeletedAt `gorm:"index"`

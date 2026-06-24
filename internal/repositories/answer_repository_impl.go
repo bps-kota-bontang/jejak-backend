17e7a77421da51115e3c72e10533a07cd32fbe7f
Arif Rahman Hakim <new.arifrh199@gmail.com>
2026-06-24 13:22:19 +0800

    feat: remove 'value' from update columns in ReplaceByAssignmentID method

--- internal/repositories/answer_repository_impl.go
+++ internal/repositories/answer_repository_impl.go
@@ -28,7 +28,6 @@ func (r *AnswerRepositoryImpl) ReplaceByAssignmentID(assignmentID string, answer
 			},
 			DoUpdates: clause.AssignmentColumns([]string{
 				"location_id",
-				"value",
 				"answered_at",
 				"revised_at",
 				"updated_at",
